30128fae5778f87ac8a91fb88fd92ed82d9bbf4a
Yassine <yassine.boufnichel@adria-bt.com>
2025-07-09 21:51:14 +0100

    Initial Commit

---
+++ WhetherAppTests/WhetherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhetherAppTests.swift
+//  WhetherAppTests
+//
+//  Created by MAC on 9/7/2025.
+//
+
+import Testing
+@testable import WhetherApp
+
+struct WhetherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ WhetherAppUITests/WhetherAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  WhetherAppUITestsLaunchTests.swift
+//  WhetherAppUITests
+//
+//  Created by MAC on 9/7/2025.
+//
+
+import XCTest
+
+final class WhetherAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
